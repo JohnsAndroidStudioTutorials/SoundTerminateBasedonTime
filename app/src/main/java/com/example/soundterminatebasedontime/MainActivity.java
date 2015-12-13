79224b8bf4a4063b1a90c1461eb4e6911c5a22b5
John Sartain <jhnsartain3@gmail.com>
2015-12-13 12:57:05 -0600

    Initial commit

---
+++ app/src/main/java/com/example/soundterminatebasedontime/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.soundterminatebasedontime;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.car_alarm);
+        Button playCarAlarmFull = (Button) findViewById(R.id.car_alarm_full);
+        Button playCarAlarmFiveSeconds = (Button) findViewById(R.id.car_alarm_five_seconds);
+
+        playCarAlarmFull.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+
+        playCarAlarmFiveSeconds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+    }
+}
